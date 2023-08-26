8e3f1c7fada01652052696494a5f8f485ac6c3e2
Мик <mishlmish@inbox.ru>
2023-08-26 10:31:18 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,15 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rsl = -1;
+        for (int i = start; i < finish + 1; i++) {
+            if (data[i] == el) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -32,4 +32,59 @@ class FindLoopTest {
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void  whenDiapasonHas8ThanResultEqualFinish() {
+        int[] data = {5, 2, 10, 2, 4, 8, 12, 4, 3, 25, 16};
+        int el = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void  whenDiapasonHas4ThanResultEqualStart() {
+        int[] data = {5, 2, 10, 2, 4, 8, 2, 4, 3, 2, 2};
+        int el = 4;
+        int start = 4;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void  whenDiapasonHasSomeEqualElementThan1() {
+        int[] data = {5, 2, 10, 2, 4, 8, 2, 4, 3, 2, 2};
+        int el = 2;
+        int start = 1;
+        int finish = 7;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void  whenDiapasonHas2Than() {
+        int[] data = {5, 2, 10, 2, 4, 8, 2, 4, 3, 2, 2};
+        int el = 2;
+        int start = 4;
+        int finish = 9;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void  whenDiapasonHasNot7ThanMinus1() {
+        int[] data = {5, 2, 10, 2, 4, 8, 2, 4, 3, 2, 2};
+        int el = 7;
+        int start = 4;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
 }
