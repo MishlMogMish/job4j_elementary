c3e4309998a9fb381993c15bdf64bad975e8ec35
Мик <mishlmish@inbox.ru>
2023-09-03 07:17:42 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,12 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public static int max(int left, int middle, int right) {
+        return max(left, max(middle, right));
+    }
+
+    public static int max(int left, int midleft, int midright, int right) {
+        return max(left, max(midleft, max(midright, right)));
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class MaxTest {
@@ -31,4 +32,47 @@ class MaxTest {
         int expected = 15;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax1to15to34Then34() {
+        int left = 1;
+        int middle = 15;
+        int right = 34;
+        int result = Max.max(left, middle, right);
+        int expected = 34;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax100to15to34Then34() {
+        int left = 100;
+        int middle = 15;
+        int right = 34;
+        int result = Max.max(left, middle, right);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax100to15to34to3Then34() {
+        int left = 100;
+        int midleft = 15;
+        int midright = 34;
+        int right = 3;
+        int result = Max.max(left, midleft, midright, right);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax100to15to34to300Then34() {
+        int left = 100;
+        int midleft = 15;
+        int midright = 34;
+        int right = 300;
+        int result = Max.max(left, midleft, midright, right);
+        int expected = 300;
+        assertThat(result).isEqualTo(expected);
+    }
+
 }
